47f5330bbc39da817932d7dff595d03cf75e8a74
nadeemabbas0474 <nadeemabbaso474@gmail.com>
2022-12-22 22:05:39 +0500

    p tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Heading } from '@chakra-ui/react';
+import { Typography } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Heading>I'm a Heading</Heading>
+    <Typography>this is Typography</Typography>
     </div>
   );
 }
